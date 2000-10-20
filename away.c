189a8efe802eae30e3988e4e3ffcfdb12ba0c499
cameron <cameron>
2000-10-20 02:36:01 +0000

    Moved functions from header file to source file Changed some global variables Added getopt functionality Renamed exec_as_message() to re_execute() and enhanced it Cleaned up help output and added print_version() function Moved the bulk of main() into a master() function Added a WIP function named set_conf_path()

--- away.c
+++ away.c
@@ -19,24 +19,85 @@
 
 #include "away.h"
 
-int main(int argc, char *argv[]) {
-  if ((argc > 2) && (strcmp(argv[1],"-m") == 0)) { exec_as_mesg(argc, argv); }
-  else if ((argc == 2) && (strcmp(argv[1],"--help") == 0)) { ext_help(); }
-  else if ((argc == 2) && (strcmp(argv[1],"-m") == 0)) { short_help(); }
-  else if (argc == 1) { short_help(); }
-  else {
+int main(int argc, char **argv) {
+  int option_count = 0;
+  int option_index = 0;
+  int c = 0;
+  short mesg_exec = 1;
+  short restart = 0;
+
+  signal(SIGINT , SIG_IGN);
+  signal(SIGTERM, SIG_IGN);
+  signal(SIGQUIT, SIG_IGN);
+  signal(SIGTSTP, SIG_IGN);
+  signal(SIGUSR1, SIG_IGN);
+  signal(SIGUSR2, SIG_IGN);
+ 
+  if (argc == 1) { short_help(argv[0]); }
+  while ((c = getopt_long(argc, argv, "c:mhpPw:Wv", long_options, &option_index)) && c != -1) {
+    restart = 1;
+    switch (c) {
+      case 'c':
+        if (! set_conf_path(optarg)) {
+          fprintf(stderr, "Cannot set path to conf file: %.200s", optarg);
+          return 1;
+        }
+        option_count++;
+        if (! strchr(argv[option_count],'=')) { option_count++; }
+        break;
+      case 'm':
+        mesg_exec = 0;
+        option_count++;
+        break;
+      case 'h':
+        ext_help(argv[0]);
+        break;
+      case 'p':
+        PERSIST = PERSIST_OP = 1;
+        option_count++;
+        break;
+      case 'P':
+        PERSIST = 0;
+        PERSIST_OP = 1;
+        option_count++;
+        break;
+      case 'w':
+        if (atoi(optarg) >= MIN_WAIT_SECS) WAIT_SECS = atoi(optarg);
+        WAIT_OP = 1;
+        option_count++;
+        if (! strchr(argv[option_count],'=')) { option_count++; }
+        break;
+      case 'W':
+        WAIT_OP = 1;
+        option_count++;
+        break;
+      case 'v':
+        print_version();
+        break;
+      default:
+        break;
+    }
+  }
+
+  /* if passed an option, we need to re-exec */
+  if (restart) { re_exec(argc, argv, option_count, mesg_exec); }
+
+printf("Wait   : %d\n", WAIT_SECS);
+printf("Persist: %d\n", PERSIST);
+printf("ZZzz...\n");
+sleep(3);
+return 0;
+
+  master();
+  return 0;
+}
+
+void master(void) {
     pthread_t mail_thread;
     short error = 1;
     struct passwd *pw = NULL;
     Mailbox *mailboxRoot = NULL, *mb = NULL;
 
-    signal(SIGINT , SIG_IGN);
-    signal(SIGTERM, SIG_IGN);
-    signal(SIGQUIT, SIG_IGN);
-    signal(SIGTSTP, SIG_IGN);
-    signal(SIGUSR1, SIG_IGN);
-    signal(SIGUSR2, SIG_IGN);
- 
     /* get home dir */
     pw = getpwuid(getuid());
     if (!pw) { fprintf(stderr, "You don't exist!\n"); exit(1); }
@@ -77,6 +138,391 @@ int main(int argc, char *argv[]) {
       free(mb);
       mb = next;
     };
+//  }
+}
+
+
+/* Authenticate Password */
+short authenticate(char *username) {
+  short error;
+  pam_handle_t *pamh = NULL;
+
+  /* start PAM session for 'away'.          *
+   * the session name is what PAM looks for *
+   * in /etc/pam.d/ or /etc/pam.config      */
+  error = pam_start("away", username, &conv, &pamh);
+
+  /* authenticate user */
+  if (!error) error = pam_authenticate(pamh, 0);
+
+  /* close PAM session */
+  if (pam_end(pamh,error) != PAM_SUCCESS) {
+    pamh = NULL;
+    fprintf(stderr,"away: ** failed to release PAM authenticator\n");
+    error = 1;
   }
-  return 0;
+  return error;
+}
+
+/* Make a Path */
+char *make_path(char *dirs, char *filename) {
+  unsigned int size = 8;
+  char *buffer = (char *) malloc (size),
+    *last = strrchr(dirs, '/'), *format = NULL;
+
+  if (last != 0 && strlen(last) == 1) format = "%s%s";
+  else format = "%s/%s";
+
+  while (1) {
+    /* try to print in the allocated space. */
+    int nchars = snprintf (buffer, size, format, dirs, filename);
+    /* if that worked, return the string. */
+    if (nchars < size) return buffer;
+    /* else try again with twice as much space. */
+    size *= 2;
+    buffer = (char *) realloc (buffer,size);
+  }
+}
+
+/* Version */
+void print_version(void) {
+  fprintf(stderr, "away v"
+    VERSION " (c) Cameron Moore <cameron@unbeatenpath.net>\n");
+  exit (0);
+}
+
+/* Short Help */
+void short_help(char *argv0) {
+  fprintf(stderr,"Usage: %s [OPTIONS] <REASON>\n", argv0);
+  exit (0);
+}
+
+/* Extended Help */
+void ext_help(char *argv0) {
+  printf("Usage: %s [OPTIONS] <REASON>\n", argv0);
+  printf("A terminal locking program.\n\n");
+  printf("  -c, --conf=FILE         specifies an alternative configuration file\n");
+  printf("  -h, --help              display this help information\n");
+  printf("  -m, --message           execute showing only the message passed\n");
+  printf("                          to the program\n");
+  printf("  -p, --persist           continue checking for new mail as long as\n");
+  printf("                          there is at least one mailbox that has not\n");
+  printf("                          recieved new mail\n");
+  printf("  -P, --nopersist         stop checking mail if any mailbox is found\n");
+  printf("                          to have new mail\n");
+  printf("  -w, --wait=SECS         sets how long the program waits between\n");
+  printf("                          mail checks\n");
+  printf("  -W, --nowait            use the default wait time of %d seconds\n",
+         WAIT_SECS);
+  printf("  -v, --version           display version information\n");
+  printf("\naway v" VERSION " (c) Cameron Moore <cameron@unbeatenpath.net>\n");
+  exit (0);
+}
+
+/* Stall */
+void stall(void) {
+  printf("\n       You went away at %.20s",awayTime);
+  printf("\n\n -- Press [Enter] to come back online --\n");
+  getchar();
+}
+
+/* Get Current Time */
+char *make_time(void) {
+  unsigned int size = 8;
+  char *buffer = (char *) malloc (size);
+  time_t t = time(NULL);
+  struct tm *thetime = localtime(&t);
+
+  while (1) {
+    /* try to print in the allocated space. */
+    int nchars = snprintf (buffer, size, "%.02d:%.02d:%.02d",
+                           thetime->tm_hour, thetime->tm_min, thetime->tm_sec);
+    /* if that worked, return the string. */
+    if (nchars < size) return buffer;
+    /* else try again with twice as much space. */
+    size *= 2;
+    buffer = (char *) realloc (buffer,size);
+  }
+}
+
+/* Salutations */
+void salutations(void) {
+  printf("\n     Welcome back. It's %.20s\n\n", make_time());
+}
+
+/* Check for New Mail */
+short new_mail(char *path) {
+  struct stat status;
+  time_t mailread = 0, mailrecv = 0;
+
+  if (stat(path, &status) < 0) {
+    printf("Could not get status of \"%.200s\"\n", path);
+    /* if we can't get a status on the mail file, *
+     * then our thread is useless.  so kill it.   */
+    mailFound = 0;
+    pthread_exit(NULL);
+  }
+  else if (status.st_size != 0) {
+    mailrecv = status.st_mtime;
+    mailread = status.st_atime;
+  }
+
+  if (mailread < mailrecv)
+    return mailFound = 1;
+  else
+    return mailFound = 0;
+}
+
+/* Mail Thread Function */
+void mail_thread_f(Mailbox *root) {
+  Mailbox *mb = NULL;
+  short found_mail = 0;
+
+  while (!found_mail) {
+    for (mb = root; !found_mail && mb != NULL; mb = mb->next) {
+      if (new_mail(mb->path)) {
+        found_mail = 1;
+        foundIn = mb->desc;
+      }
+    }
+    if (!found_mail) sleep(WAIT_SECS);
+  }
+  /* sleep for 1 second to make sure the *
+   * main process outputs its stuff...   */
+  sleep(1);
+
+  /* don't interrupt user authentication */
+  if (!pamActive) {
+    printf("\a\n       You have new mail in %.200s.\n", foundIn);
+    notified = 1;
+  }
+  /* exit the thread */
+  pthread_exit(NULL);
+}
+
+/* Get CmdCode */
+static CmdCodes get_opcode(const char *cp, const char *filename, int linenum) {
+  unsigned int i;
+  for (i = 0; commands[i].name; i++)
+    if (strcasecmp(cp, commands[i].name) == 0)
+      return commands[i].opcode;
+  fprintf(stderr, "%.200s: line %d: Bad configuration option: %.200s\n",
+    filename, linenum, cp);
+  return oBadCmd;
+}
+
+/* Allocate New Link Node */
+static void *my_malloc(int n) {
+  void *p = malloc( (size_t)n );
+  if (p == NULL) {
+    fprintf(stderr, "Out of Memory");
+    exit(1);
+  } return p;
+}
+
+/* Setup New Mailbox Container */
+static Mailbox *make_cell(void) {
+  Mailbox *m = (Mailbox *) my_malloc(sizeof(Mailbox));
+  m->path = m->desc = NULL;
+  m->next = NULL;
+  return m;
+}
+
+/* Find New Node and Set String Values */
+static Mailbox *snocString(Mailbox *root, char *path, char *desc) {
+  if (root == NULL) {
+    Mailbox *tmp = make_cell();
+    tmp->path = (char *) my_malloc(5 + strlen(path));
+    tmp->desc = (char *) my_malloc(5 + strlen(desc));
+    strcpy(tmp->path, path);
+    strcpy(tmp->desc, desc);
+    return tmp;
+  } else {
+    Mailbox *tmp = root;
+    while (tmp->next != NULL) tmp = tmp->next;
+    tmp->next = snocString(tmp->next, path, desc);
+    return root;
+  }
+}
+
+/* Add New Mailbox to List */
+static void add_mailbox(Mailbox **list, char *path, char *desc) {
+  if (path != NULL) {
+    int i, j, k, gotPath = 0;
+    char tmpPath[FILE_NAME_LEN], tmpDesc[FILE_NAME_LEN];
+
+    i = 0;
+    while (1) {
+      if (path[i] == 0) break;
+      path += i;
+      i = 0;
+      while (isspace((int)(path[0]))) path++;
+      while (path[i] != 0 && !isspace((int)(path[i]))) i++;
+      if (i > 0) {
+        k = i; if (k > FILE_NAME_LEN-10) k = FILE_NAME_LEN-10;
+        for (j = 0; j < k; j++) tmpPath[j] = path[j];
+        tmpPath[k] = 0;
+        gotPath = 1;
+      }
+    }
+    i = 0;
+    while (gotPath) {
+      if (desc[i] == 0) break;
+      desc += i;
+      i = 0;
+      while (isspace((int)(desc[0]))) desc++;
+      while (desc[i] != 0) i++;
+      if (i > 0) {
+        k = i; if (k > FILE_NAME_LEN-10) k = FILE_NAME_LEN-10;
+        for (j = 0; j < k; j++) tmpDesc[j] = desc[j];
+        tmpDesc[k] = 0;
+        APPEND_MAILBOX(*list, tmpPath, tmpDesc);
+      }
+    }
+  }
+}
+
+/* Set Defaults */
+void set_defaults(Mailbox **root, char *name) {
+  add_mailbox(root, make_path(_PATH_MAILDIR, name), "INBOX"); }
+
+/* Read Configuration File */
+void read_config(Mailbox **root, char *homedir, char *username) {
+  FILE *f = NULL;
+  char line[1024], filename[256];
+  char *cp = NULL, *maildir = "";
+  unsigned int linenum = 0, opcode;
+  short changed_wait_secs = 0;
+
+  snprintf(filename, sizeof filename, "%.100s/%.100s", homedir,conf_file);
+  /* open rc file */
+  f = fopen(filename, "r");
+  if (!f) {
+    /* if there is no rc file, assume they have a /var/spool/mail file */
+    set_defaults(root, username);
+    return;
+  }
+
+  /* go line by line */
+  while (fgets(line, sizeof(line), f)) {
+    linenum++;
+    /* skip whitespace */
+     cp = line + strspn(line, WHITESPACE);
+    if (!*cp || *cp == '\n' || *cp == '#');
+    else {
+      /* get opcode */
+      cp = strtok(cp, WHITESPACE);
+      opcode = get_opcode(cp, filename, linenum);
+
+      switch (opcode) {
+      case oMaildir:
+        /* save the maildir for building paths to bailboxes */
+        cp = strtok(NULL, WHITESPACE);
+        if (!cp)
+          fprintf(stderr,"%.200s line %d: missing argument.\n",
+                  filename, linenum);
+        else if (strcmp(cp,maildir) != 0) 
+          if (cp[0] == '~') {
+            /* handle references to home dir as ~, ~foo, etc. */
+            char *save = NULL;
+            cp = strtok_r(cp,"~",&save);
+            if (strstr(cp, "/") != NULL)
+              maildir = strtok_r(cp, "/",&save);
+            else maildir = cp;
+            if (maildir != NULL && strcmp(maildir,username) == 0)
+              cp = strcat(cp=strdup("/"), save);
+            if (cp == NULL) cp = homedir;
+            else cp = strcat(homedir, cp);
+          }
+          maildir = make_path(cp,"");
+        break;
+
+      case oMailbox:
+        /* append mailbox to maildir and save */
+        cp = strtok(NULL, WHITESPACE);
+        if (!cp)
+          fprintf(stderr,"%.200s line %d: missing argument.\n",
+                  filename, linenum);
+        else if (strcmp(maildir,"") != 0) {
+          char *desc = strtok(NULL, "\n");
+          if (!desc) desc = strdup(cp);
+          else if (strstr(desc,"{") && strstr(desc,"}"))
+            desc = strtok(desc,"{}");
+          else {
+            fprintf(stderr,"%.200s line %d: garbage at end of line.\n",
+                    filename, linenum);
+            desc = strdup(cp);
+          }
+          add_mailbox(root, make_path(maildir,cp), desc);
+        } else {
+          fprintf(stderr,
+                  "%.200s line %d: Mailbox option needs a Maildir parent.\n",
+                  filename, linenum);
+        }
+        break;
+
+      case oWait:
+        /* if changed from command line */
+        if (WAIT_OP) break;
+        /* change WAIT_SECS */
+        if (changed_wait_secs) {
+          cp = strtok(NULL, WHITESPACE);
+          fprintf(stderr,"%.200s line %d: multiple Wait commands.\n",
+                  filename, linenum);
+        } else {
+          cp = strtok(NULL, WHITESPACE);
+          if (!cp)
+            fprintf(stderr,"%.200s line %d: missing argument.\n",
+                    filename, linenum);
+          else if (atoi(cp) >= MIN_WAIT_SECS) WAIT_SECS = atoi(cp);
+          else {
+            fprintf(stderr,
+                    "%.200s line %d: Wait value less than minimum (%d).\n",
+                    filename, linenum, MIN_WAIT_SECS);
+            WAIT_SECS = MIN_WAIT_SECS;
+          }
+          changed_wait_secs = 1;
+        }
+        break;
+
+      default:
+        break;
+      } /* switch */
+
+      /* check for garbage at EOL */
+      if (strtok(NULL, WHITESPACE) != NULL)
+        fprintf(stderr,"%.200s line %d: garbage at end of line.\n",
+                filename, linenum);
+    } /* else */
+  } /* while */
+  fclose(f);
+}
+
+/* Re-execute */
+void re_exec(int argc, char *argv[], int opt_cnt, short as_mesg) {
+  /* preserve location of binary */
+  char *original = argv[0]; 
+  /* create new argv */
+  char *newargv[argc];
+
+  int i = (as_mesg ? 0 : 1);
+  for (; i < argc-(opt_cnt+as_mesg); i++)
+    newargv[i] = argv[i+(opt_cnt+as_mesg)];
+
+  /* keep original exec name */
+  if (!as_mesg) { newargv[0] = argv[0]; }
+
+  /* pad with space to make use of one word arguments to -m */
+  newargv[argc-(opt_cnt+as_mesg)] = " ";
+  i = opt_cnt - (as_mesg ? 0 : 1);
+  for(; i > 0; i--) { newargv[argc-i] = NULL; }
+
+  /* exec THIS! */
+  execvp(original, newargv);
+}
+
+/* Set conf file path */
+short set_conf_path(char *path) {
+  printf("** set_conf_path(%.200s);\n", path);
+  return 1;
 }
--- away.h
+++ away.h
@@ -17,33 +17,50 @@
  * Or try here: http://www.fsf.org/copyleft/gpl.html
  */
 
-#ifndef _AWAY_H_
-#define _AWAY_H_
+#ifndef _AWAY_H
+#define _AWAY_H
 
-#include <security/pam_appl.h>
-#include <security/pam_misc.h>
-#include <sys/stat.h>
-#include <pthread.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <paths.h>
+/* Away version number */
+#define VERSION "0.9.1"
+
+/* Separator for conf file */
+#define WHITESPACE " \t\n"
+
+/* macro */
+#define APPEND_MAILBOX(root,path,desc) root=snocString((root),(path),(desc))
+
+/* Max filename length */
+#define FILE_NAME_LEN 1024
+
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <paths.h>
 #include <time.h>
 #include <pwd.h>
 #include <ctype.h>
+#include <signal.h>
 #include <getopt.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <security/pam_appl.h>
+#include <security/pam_misc.h>
 
-#define VERSION "0.9.0"
-#define AWAY_CONF_FILE ".awayrc"
-#define WHITESPACE " \t\n"
-#define APPEND_MAILBOX(root,path,desc) root=snocString((root),(path),(desc))
-#define FILE_NAME_LEN 1024
+/* Default conf file name */
+char *conf_file = ".awayrc";
 
-/* number of secs to wait between checking mailboxes for new mail */
+/* min mail check interval */
 const int MIN_WAIT_SECS = 30;
+
+/* ... */
 int WAIT_SECS = 300;
+int PERSIST = 1;
+
+/* flags to allow command line options to override conf file options */
+short WAIT_OP = 0;
+short PERSIST_OP = 0;
 
 /* global variables */
 char *foundIn = NULL, *awayTime = 0;
@@ -71,355 +88,38 @@ typedef struct mailbox {
   struct mailbox *next;
 } Mailbox;
 
-/* * * * * * * */
-
-/* Authenticate Password */
-short authenticate(char *username) {
-  short error;
-  pam_handle_t *pamh = NULL;
-
-  /* start PAM session for 'away'.          *
-   * the session name is what PAM looks for *
-   * in /etc/pam.d/ or /etc/pam.config      */
-  error = pam_start("away", username, &conv, &pamh);
-
-  /* authenticate user */
-  if (!error) error = pam_authenticate(pamh, 0);
-
-  /* close PAM session */
-  if (pam_end(pamh,error) != PAM_SUCCESS) {
-    pamh = NULL;
-    fprintf(stderr,"away: ** failed to release PAM authenticator\n");
-    error = 1;
-  }
-  return error;
-}
-
-/* Make a Path */
-char *make_path(char *dirs, char *filename) {
-  unsigned int size = 8;
-  char *buffer = (char *) malloc (size),
-    *last = strrchr(dirs, '/'), *format = NULL;
-
-  if (last != 0 && strlen(last) == 1) format = "%s%s";
-  else format = "%s/%s";
-
-  while (1) {
-    /* try to print in the allocated space. */
-    int nchars = snprintf (buffer, size, format, dirs, filename);
-    /* if that worked, return the string. */
-    if (nchars < size) return buffer;
-    /* else try again with twice as much space. */
-    size *= 2;
-    buffer = (char *) realloc (buffer,size);
-  }
-}
-
-/* Short Help */
-void short_help() { fprintf(stderr,"Usage: away [--help] [-m] <REASON>\n"); }
-
-/* Extended Help */
-void ext_help() {
-  printf("Away v" VERSION " (c) Cameron Moore <cameron@unbeatenpath.net>\n");
-  printf("Usage: away [--help] [-m] <REASON>\n\n");
-  printf("   ex: away playing games\\!\\!\n");
-  printf("       away -m Zzz...  :\\)\n\n");
-  printf("   Use -m to have away appear to execute as the first word\n");
-  printf("   in <REASON>.\n\n");
-  printf("   If you plan on using special characters, make sure you\n");
-  printf("   escape each character like the examples above.\n\n");
-  printf("   Compiled to check mail every %d seconds.\n\n", WAIT_SECS);
-}
-
-/* Stall */
-void stall() {
-  printf("\n       You went away at %.20s",awayTime);
-  printf("\n\n -- Press [Enter] to come back online --\n");
-  getchar();
-}
-
-/* Get Current Time */
-char *make_time() {
-  unsigned int size = 8;
-  char *buffer = (char *) malloc (size);
-  time_t t = time(NULL);
-  struct tm *thetime = localtime(&t);
-
-  while (1) {
-    /* try to print in the allocated space. */
-    int nchars = snprintf (buffer, size, "%.02d:%.02d:%.02d",
-                           thetime->tm_hour, thetime->tm_min, thetime->tm_sec);
-    /* if that worked, return the string. */
-    if (nchars < size) return buffer;
-    /* else try again with twice as much space. */
-    size *= 2;
-    buffer = (char *) realloc (buffer,size);
-  }
-}
-
-/* Salutations */
-void salutations() {
-  printf("\n     Welcome back. It's %.20s\n\n", make_time());
-}
-
-/* Check for New Mail */
-short new_mail(char *path) {
-  struct stat status;
-  time_t mailread = 0, mailrecv = 0;
-
-  if (stat(path, &status) < 0) {
-    printf("Could not get status of \"%.200s\"\n", path);
-    /* if we can't get a status on the mail file, *
-     * then our thread is useless.  so kill it.   */
-    mailFound = 0;
-    pthread_exit(NULL);
-  }
-  else if (status.st_size != 0) {
-    mailrecv = status.st_mtime;
-    mailread = status.st_atime;
-  }
-
-  if (mailread < mailrecv)
-    return mailFound = 1;
-  else
-    return mailFound = 0;
-}
-
-/* Mail Thread Function */
-void mail_thread_f(Mailbox *root) {
-  Mailbox *mb = NULL;
-  short found_mail = 0;
-
-  while (!found_mail) {
-    for (mb = root; !found_mail && mb != NULL; mb = mb->next) {
-      if (new_mail(mb->path)) {
-        found_mail = 1;
-        foundIn = mb->desc;
-      }
-    }
-    if (!found_mail) sleep(WAIT_SECS);
-  }
-  /* sleep for 1 second to make sure the *
-   * main process outputs its stuff...   */
-  sleep(1);
-
-  /* don't interrupt user authentication */
-  if (!pamActive) {
-    printf("\a\n       You have new mail in %.200s.\n", foundIn);
-    notified = 1;
-  }
-  /* exit the thread */
-  pthread_exit(NULL);
-}
-
-/* Get CmdCode */
-static CmdCodes get_opcode(const char *cp, const char *filename, int linenum) {
-  unsigned int i;
-  for (i = 0; commands[i].name; i++)
-    if (strcasecmp(cp, commands[i].name) == 0)
-      return commands[i].opcode;
-  fprintf(stderr, "%.200s: line %d: Bad configuration option: %.200s\n",
-    filename, linenum, cp);
-  return oBadCmd;
-}
-
-/* Allocate New Link Node */
-static void *my_malloc(int n) {
-  void *p = malloc( (size_t)n );
-  if (p == NULL) {
-    fprintf(stderr, "Out of Memory");
-    exit(1);
-  } return p;
-}
-
-/* Setup New Mailbox Container */
-static Mailbox *make_cell(void) {
-  Mailbox *m = (Mailbox *) my_malloc(sizeof(Mailbox));
-  m->path = m->desc = NULL;
-  m->next = NULL;
-  return m;
-}
-
-/* Find New Node and Set String Values */
-static Mailbox *snocString(Mailbox *root, char *path, char *desc) {
-  if (root == NULL) {
-    Mailbox *tmp = make_cell();
-    tmp->path = (char *) my_malloc(5 + strlen(path));
-    tmp->desc = (char *) my_malloc(5 + strlen(desc));
-    strcpy(tmp->path, path);
-    strcpy(tmp->desc, desc);
-    return tmp;
-  } else {
-    Mailbox *tmp = root;
-    while (tmp->next != NULL) tmp = tmp->next;
-    tmp->next = snocString(tmp->next, path, desc);
-    return root;
-  }
-}
-
-/* Add New Mailbox to List */
-static void add_mailbox(Mailbox **list, char *path, char *desc) {
-  if (path != NULL) {
-    int i, j, k, gotPath = 0;
-    char tmpPath[FILE_NAME_LEN], tmpDesc[FILE_NAME_LEN];
-
-    i = 0;
-    while (1) {
-      if (path[i] == 0) break;
-      path += i;
-      i = 0;
-      while (isspace((int)(path[0]))) path++;
-      while (path[i] != 0 && !isspace((int)(path[i]))) i++;
-      if (i > 0) {
-        k = i; if (k > FILE_NAME_LEN-10) k = FILE_NAME_LEN-10;
-        for (j = 0; j < k; j++) tmpPath[j] = path[j];
-        tmpPath[k] = 0;
-        gotPath = 1;
-      }
-    }
-    i = 0;
-    while (gotPath) {
-      if (desc[i] == 0) break;
-      desc += i;
-      i = 0;
-      while (isspace((int)(desc[0]))) desc++;
-      while (desc[i] != 0) i++;
-      if (i > 0) {
-        k = i; if (k > FILE_NAME_LEN-10) k = FILE_NAME_LEN-10;
-        for (j = 0; j < k; j++) tmpDesc[j] = desc[j];
-        tmpDesc[k] = 0;
-        APPEND_MAILBOX(*list, tmpPath, tmpDesc);
-      }
-    }
-  }
-}
-
-/* Set Defaults */
-void set_defaults(Mailbox **root, char *name) {
-  add_mailbox(root, make_path(_PATH_MAILDIR, name), "INBOX");
-}
-
-/* Read Configuration File */
-void read_config(Mailbox **root, char *homedir, char *username) {
-  FILE *f = NULL;
-  char line[1024], filename[256];
-  char *cp = NULL, *maildir = "";
-  unsigned int linenum = 0, opcode;
-  short changed_wait_secs = 0;
-
-  snprintf(filename, sizeof filename, "%.100s/%.100s", homedir, AWAY_CONF_FILE);
-  /* open rc file */
-  f = fopen(filename, "r");
-  if (!f) {
-    /* if there is no rc file, assume they have a /var/spool/mail file */
-    set_defaults(root, username);
-    return;
-  }
-
-  /* go line by line */
-  while (fgets(line, sizeof(line), f)) {
-    linenum++;
-    /* skip whitespace */
-     cp = line + strspn(line, WHITESPACE);
-    if (!*cp || *cp == '\n' || *cp == '#');
-    else {
-      /* get opcode */
-      cp = strtok(cp, WHITESPACE);
-      opcode = get_opcode(cp, filename, linenum);
-
-      switch (opcode) {
-      case oMaildir:
-        /* save the maildir for building paths to bailboxes */
-        cp = strtok(NULL, WHITESPACE);
-        if (!cp)
-          fprintf(stderr,"%.200s line %d: missing argument.\n",
-                  filename, linenum);
-        else if (strcmp(cp,maildir) != 0) 
-          if (cp[0] == '~') {
-            /* handle references to home dir as ~, ~foo, etc. */
-            char *save = NULL;
-            cp = strtok_r(cp,"~",&save);
-            if (strstr(cp, "/") != NULL)
-              maildir = strtok_r(cp, "/",&save);
-            else maildir = cp;
-            if (maildir != NULL && strcmp(maildir,username) == 0)
-              cp = strcat(cp=strdup("/"), save);
-            if (cp == NULL) cp = homedir;
-            else cp = strcat(homedir, cp);
-          }
-          maildir = make_path(cp,"");
-        break;
-
-      case oMailbox:
-        /* append mailbox to maildir and save */
-        cp = strtok(NULL, WHITESPACE);
-        if (!cp)
-          fprintf(stderr,"%.200s line %d: missing argument.\n",
-                  filename, linenum);
-        else if (strcmp(maildir,"") != 0) {
-          char *desc = strtok(NULL, "\n");
-          if (!desc) desc = strdup(cp);
-          else if (strstr(desc,"{") && strstr(desc,"}")) desc = strtok(desc,"{}");
-          else {
-            fprintf(stderr,"%.200s line %d: garbage at end of line.\n", filename, linenum);
-            desc = strdup(cp);
-          }
-          add_mailbox(root, make_path(maildir,cp), desc);
-        } else
-          fprintf(stderr,"%.200s line %d: Mailbox option needs a Maildir parent.\n",
-          filename, linenum);
-        break;
-
-      case oWait:
-        /* change WAIT_SECS */
-        if (changed_wait_secs) {
-          cp = strtok(NULL, WHITESPACE);
-          fprintf(stderr,"%.200s line %d: multiple Wait commands.\n",
-                  filename, linenum);
-        } else {
-          cp = strtok(NULL, WHITESPACE);
-          if (!cp)
-            fprintf(stderr,"%.200s line %d: missing argument.\n",
-                    filename, linenum);
-          else if (atoi(cp) >= MIN_WAIT_SECS) WAIT_SECS = atoi(cp);
-          else {
-            fprintf(stderr,"%.200s line %d: Wait value less than minimum (%d).\n",
-                    filename, linenum, MIN_WAIT_SECS);
-            WAIT_SECS = MIN_WAIT_SECS;
-          }
-          changed_wait_secs = 1;
-        }
-        break;
-
-      default:
-        break;
-      } /* switch */
-
-      /* check for garbage at EOL */
-      if (strtok(NULL, WHITESPACE) != NULL)
-        fprintf(stderr,"%.200s line %d: garbage at end of line.\n", filename, linenum);
-    } /* else */
-  } /* while */
-  fclose(f);
-}
-
-/* Exec as Message */
-void exec_as_mesg(int argc, char *argv[])
+/* command line options for getopt */
+static struct option long_options[] =
 {
-  /* preserve location of binary */
-  char *original = argv[0]; 
-  /* create new argv */
-  char *newargv[argc];
+  {"conf", 1, 0, 'c'},
+  {"message", 0, 0, 'm'},
+  {"help", 0, 0, 'h'},
+  {"persist", 0, 0, 'p'},
+  {"wait", 1, 0, 'w'},
+  {"version", 0, 0, 'v'},
+  {0, 0, 0, 0}
+};
 
-  int i=0;
-  for (; i<=argc-2; i++){ newargv[i] = argv[i+2]; }
+/* prototypes */
+void master(void);
+short authenticate(char *username);
+char *make_path(char *dirs, char *filename);
+void print_version(void);
+void short_help(char *argv0);
+void ext_help(char *argv0);
+void stall(void);
+char *make_time(void);
+void salutations(void);
+short new_mail(char *path);
+void mail_thread_f(Mailbox *root);
+static CmdCodes get_opcode(const char *cp, const char *filename, int linenum);
+static void *my_malloc(int n);
+static Mailbox *make_cell(void);
+static Mailbox *snocString(Mailbox *root, char *path, char *desc);
+static void add_mailbox(Mailbox **list, char *path, char *desc);
+void set_defaults(Mailbox **root, char *name);
+void read_config(Mailbox **root, char *homedir, char *username);
+void re_exec(int argc, char *argv[], int opt_cnt, short as_mesg);
+short set_conf_path(char *path);
 
-  /* pad with space to make use of one word arguments to -m */
-  newargv[argc-2] = " ";
-  newargv[argc-1] = NULL;
-
-  /* exec THIS! */
-  execvp(original, newargv);
-} /* execAsMessage */
-
-#endif   /* _AWAY_H_ */
+#endif   /* _AWAY_H */
