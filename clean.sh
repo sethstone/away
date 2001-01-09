342fabf94230059c6aff6b068da460e82498c3f0
hrothgar <hrothgar>
2001-01-09 00:45:52 +0000

    Added a *little* brains to the clean script

--- clean.sh
+++ clean.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-make clean
+if [ -f Makefile ];then
+  make clean
+fi
 rm `find . -name Makefile`
 rm `find . -name Makefile.in`
 rm COPYING INSTALL aclocal.m4 config.* configure install-sh missing mkinstalldirs stamp-h*
